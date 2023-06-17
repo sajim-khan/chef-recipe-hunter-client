d4f45aee242aed0f95ed6c0f6035935bcc2232d5
sajim-khan <shossain1427@gmail.com>
2023-06-17 17:04:51 +0600

    added signup page design

--- src/Components/Share/Register.jsx
+++ src/Components/Share/Register.jsx
@@ -94,10 +94,272 @@ const Register = () => {
 
   return (
     <div>
-      
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          {/* <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <Player
+              className=" rounded-lg  sm:w-[500px] sm:h-[500px]"
+              autoplay
+              loop
+              src="https://assets1.lottiefiles.com/packages/lf20_hy4txm7l.json"
+              // style={{ height: "500px", width: "700px" }}
+            ></Player>
+          </div> */}
+
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <h1 className="text-5xl font-bold">Register now!</h1>
+
+            <form onSubmit={handleCreateUser} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Your Name"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  name="photo"
+                  id="photo"
+                  placeholder="Your Photo"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Your Email"
+                  required
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Your Password"
+                  required
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Register"
+                />
+              </div>
+            </form>
+            <div className="">
+              <div class="flex  flex-row w-96 mr-6 items-center justify-center lg:justify-start">
+                <p class="mb-0 mr-28 text-lg"></p>
+
+                <button
+                  onClick={handlegithulogin}
+                  type="button"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                  class="mx-1 h-9 w-9 rounded-full bg-primary uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                >
+                  <FaGithub className="mx-auto h-3.5 w-3.5" />
+                </button>
+
+                <button
+                  onClick={handleGooglesignIn}
+                  type="button"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                  class="mx-1 h-9 w-9 rounded-full bg-primary uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                >
+                  <FaGoogle className="mx-auto h-3.5 w-3.5" />
+                </button>
+              </div>
+              <p className="text-danger">{error} </p>
+
+              <div class="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+                <p class="mx-4 mb-0 text-center font-semibold dark:text-white">
+                  Or
+                </p>
+              </div>
+              <p className="mt-4">
+                Allready Have an Account
+                <Link className="text-cyan-600" to="/register">
+                  {" "}
+                  Login{" "}
+                </Link>{" "}
+              </p>
+            </div>
+          </div>
+          <div className="text-center lg:text-left">
+            <Player
+              className=" rounded-lg  sm:w-[500px] sm:h-[500px]"
+              autoplay
+              loop
+              src="https://assets1.lottiefiles.com/packages/lf20_hy4txm7l.json"
+              // style={{ height: "500px", width: "700px" }}
+            ></Player>
+          </div>
+        </div>
+      </div>
       <ToastContainer />
     </div>
   );
 };
 
 export default Register;
+<div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          {/* <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <Player
+              className=" rounded-lg  sm:w-[500px] sm:h-[500px]"
+              autoplay
+              loop
+              src="https://assets1.lottiefiles.com/packages/lf20_hy4txm7l.json"
+              // style={{ height: "500px", width: "700px" }}
+            ></Player>
+          </div> */}
+
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <h1 className="text-5xl font-bold">Register now!</h1>
+
+            <form onSubmit={handleCreateUser} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Your Name"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  name="photo"
+                  id="photo"
+                  placeholder="Your Photo"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Your Email"
+                  required
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Your Password"
+                  required
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Register"
+                />
+              </div>
+            </form>
+            <div className="">
+              <div class="flex  flex-row w-96 mr-6 items-center justify-center lg:justify-start">
+                <p class="mb-0 mr-28 text-lg"></p>
+
+                <button
+                  onClick={handlegithulogin}
+                  type="button"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                  class="mx-1 h-9 w-9 rounded-full bg-primary uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                >
+                  <FaGithub className="mx-auto h-3.5 w-3.5" />
+                </button>
+
+                <button
+                  onClick={handleGooglesignIn}
+                  type="button"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                  class="mx-1 h-9 w-9 rounded-full bg-primary uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                >
+                  <FaGoogle className="mx-auto h-3.5 w-3.5" />
+                </button>
+              </div>
+              <p className="text-danger">{error} </p>
+
+              <div class="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+                <p class="mx-4 mb-0 text-center font-semibold dark:text-white">
+                  Or
+                </p>
+              </div>
+              <p className="mt-4">
+                Allready Have an Account
+                <Link className="text-cyan-600" to="/register">
+                  {" "}
+                  Login{" "}
+                </Link>{" "}
+              </p>
+            </div>
+          </div>
+          <div className="text-center lg:text-left">
+            <Player
+              className=" rounded-lg  sm:w-[500px] sm:h-[500px]"
+              autoplay
+              loop
+              src="https://assets1.lottiefiles.com/packages/lf20_hy4txm7l.json"
+              // style={{ height: "500px", width: "700px" }}
+            ></Player>
+          </div>
+        </div>
+      </div>
+      
